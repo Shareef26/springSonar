37952b4d7445a8f03f1654d65386006c878486e1
shareef <mshareefahmed92@gmail.com>
2021-05-24 15:46:02 +0530

    added the security as well

--- src/main/java/com/SpringJPA/exception/EcxceptionHandler.java
+++ src/main/java/com/SpringJPA/exception/EcxceptionHandler.java
@@ -1,20 +1,24 @@
 package com.SpringJPA.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class EcxceptionHandler {
+public class EcxceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(NosuchPerson.class)
 	public ResponseEntity<Object> NosuchPersonExcpetion(NosuchPerson  e){
 		ApiError apierror=new ApiError(HttpStatus.BAD_REQUEST,e.getMessage(),  e);
 		return new ResponseEntity<Object>(apierror, HttpStatus.OK);}
 		
-	@ExceptionHandler(MethodArgumentNotValidException.class)	/*
+	//
+	/*
 	
 	*if you extend the ResponseEntityExceptionHandler then you not need to mention @ExceptionHandler(MethodArgumentNotValidException.class)
 	*it will automatically all the exception falling in it
@@ -25,13 +29,31 @@ public class EcxceptionHandler {
 	**if first name tag is present but value is null  in request it will handled by 
     and @notblank message on dto will be executed.
 	*
-	*/
-	public ResponseEntity<Object> handleInputValidation(MethodArgumentNotValidException e)	{
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	 public ResponseEntity<Object> handleInputValidation(MethodArgumentNotValidException e )	{
 		ApiError apierror=new ApiError(HttpStatus.BAD_REQUEST,"please check the request again",  e);
 		return new ResponseEntity<Object>(apierror, HttpStatus.OK);
 		
-	}
+		the above method worked fine when I did not extend ResponseEntityExceptionHandler and wrote just above method with 
+		MethodArgumentNotValidException e  parameter only. in the same method if I tried to write other params  wihtout extending 
+		ResponseEntityExceptionHandler then no compile error, but this exception was not invoked, instead 400 was thrown with no info
 		
+		when I extended ResponseEntityExceptionHandler I had to write the below method as it is then it worked.
+	}*/
+	
+	//below method works fine when extended responseEntity.
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request)
+	{
+		
+		ApiError apierror=new ApiError(HttpStatus.BAD_REQUEST,"please check the request again",  ex);
+		return new ResponseEntity<Object>(apierror, HttpStatus.OK);
+		
+	}
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<Object> generalException(Exception e){
--- src/main/java/com/SpringJPA/security/Security.java
+++ src/main/java/com/SpringJPA/security/Security.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -23,6 +24,11 @@ public class Security extends WebSecurityConfigurerAdapter{
 		 auth.userDetailsService(userDetailsService);
 		
 	}
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.cors().and().csrf().disable();
+    }
 	
 	@Bean
 	public static NoOpPasswordEncoder passwordEncoder() {
